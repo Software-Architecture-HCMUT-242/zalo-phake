31c234fdd25690d9a0df49b837298fdddd187633
quan <nhanduyquan217@gmail.com>
2025-04-12 19:25:56 +0700

    Refactor firebase

--- chat_app/app/main.py
+++ chat_app/app/main.py
@@ -37,78 +37,97 @@ def ping():
 def validate(body, key, type_origin, type_convert, error, required=False):
     # [1]: Check if body has key
     if (key not in body) and required:
-        error["description"] = f"[Error] Can't find key {key}"
+        error["description"] = f"[Error] Can't find key \"{key}\""
         log(error["description"])
         return False
 
     # [2]: Check type of request value
     if not isinstance(body[key], type_origin):
-        error["description"] = f"[Error] Key {key} is not of type {type_origin}"
+        error["description"] = f"[Error] Key \"{key}\" is not of type {type_origin}"
         log(error["description"])
         return False
 
-    # [3]: Try conversion to target type (to check number string like phone number, otp)
+    # [3]: Try conversion to target type (to check number string like phone number)
     try:
         body[key] = type_convert(body[key])
     except ValueError:
-        error["description"] = f"[Error] Cannot convert {key} to {type_convert}"
+        error["description"] = f"[Error] Cannot convert \"{key}\" to {type_convert}"
         log(error["description"])
         return False
     return True
 
 
-# FastAPI route for login
-@app.post("/auth/login")
-async def login(request: Dict[Any, Any]):
-    vData = deepcopy(request)  # Convert Pydantic model to dictionary
-    vError = {}
-    # [1]: Validate request body
-    if not validate(vData, "phone_number", str, int, vError, required=True): raise HTTPException(status_code=400,
-                                                                                                 detail=vError[
-                                                                                                     "description"])
-    if not validate(vData, "password", str, str, vError, required=True): raise HTTPException(status_code=400,
-                                                                                             detail=vError[
-                                                                                                 "description"])
-    log(f"[Debug]: Converted data:\n {vData}")
-
-    # [2]: Check if user exist in DB
-    vResponse = {}
-    database.query(f'/User/{request["phone_number"]}', response=vResponse)
-    log(f"[Debug] The response data is: {vResponse}")
-    if not vResponse["body"]:
-        log(f"[Debug] User not found in database")
-        raise HTTPException(status_code=400, detail="[Error]: User not found in database")
-
-    # TODO: Get token from database
-    token = "1234"
-    return {"success": True, "token": token, "user": vResponse}
-
-
-@app.post("/auth/register")
+@app.post("/api/auth/register", status_code=201)
 async def register(request: Dict[Any, Any]):
     vData = deepcopy(request)
     vError = {}
     # [1]: Validate request body
-    if not validate(vData, "phone_number", str, int, vError, required=True): raise HTTPException(status_code=400,
-                                                                                                 detail=vError[
-                                                                                                     "description"])
-    if not validate(vData, "name", str, str, vError, required=True): raise HTTPException(status_code=400,
-                                                                                         detail=vError["description"])
-    if not validate(vData, "password", str, str, vError, required=True): raise HTTPException(status_code=400,
-                                                                                             detail=vError[
-                                                                                                 "description"])
-    if not validate(vData, "otp", str, int, vError, required=True): raise HTTPException(status_code=400,
-                                                                                        detail=vError["description"])
+    if not validate(vData, "phone_number", str, int, vError, required=True):
+        raise HTTPException(status_code=400, detail=vError["description"])
+    if not validate(vData, "name", str, str, vError, required=True):
+        raise HTTPException(status_code=400, detail=vError["description"])
+    if not validate(vData, "password", str, str, vError, required=True): 
+        raise HTTPException(status_code=400, detail=vError["description"])
     log(f"[Debug]: Converted data:\n {vData}")
 
     # [2]: Check if user exist in DB
-    vResponse = {}
-    database.query(f'/User/{request["phone_number"]}', response=vResponse)
-    log(f"[Debug] The response data is: {vResponse}")
-    if vResponse["body"]:
-        log(f'[Debug] User already exist in database: {vResponse["body"]}')
-        raise HTTPException(status_code=400, detail="[Error]: User already exist in database")
+    user = database.query_user_by_phone_number(request["phone_number"])
+    log(f"[Debug] Queried user is: {user}")
+    if user:
+        log(f'[Error] User already exist in database')
+        raise HTTPException(status_code=409, detail="[Error]: User already exist in database")
 
-    # [3]: Insert user to DB if not existed
-    database.insert(f'/User/{request["phone_number"]}', {"name": request["name"], "password": request["password"]})
-    return {"success": True, "token": "1234", "user": "defaultUser"}
+    # [3]: Validate FE token from firebase OTP
+    decoded_token = database.verify_token(request["token"])
+    if not decoded_token:
+        log(f'[Error] OTP token not valid: {decoded_token}')
+        raise HTTPException(status_code=401, detail="[Error]: OTP token not valid")
+
+    # [4]: Insert user to DB if not existed
+    user = database.create_user(phone_number=request["phone_number"], password=request["password"])
+    return {"success": True, "token": "1234", "user": user}
+
+
+@app.post("/api/auth/login", status_code=200)
+async def login(request: Dict[Any, Any]):
+    vData = deepcopy(request)
+    vError = {}
+    # [1]: Validate request body
+    if not validate(vData, "phone_number", str, int, vError, required=True):
+        raise HTTPException(status_code=400, detail=vError["description"])
+    if not validate(vData, "password", str, str, vError, required=True):
+        raise HTTPException(status_code=400, detail=vError["description"])
+    log(f"[Debug]: Converted data:\n {vData}")
+
+    # [2]: Check if user exist in DB
+    user = database.query_user_by_phone_number(request["phone_number"])
+    log(f"[Debug] Queried user is: {user}")
+    if not user:
+        log(f"[Debug] User phone number \"{request["phone_number"]}\" not found in database")
+        # return obscured error info to make it harder to attack
+        raise HTTPException(status_code=401, detail="[Error]: Invalid credentials")
+    
+    # [3]: Validate password
+    if user["password"] != request["password"]:
+        log(f"[Debug] User password \"{request["password"]}\" not match database \"{user["password"]}\"")
+        # return obscured error info to make it harder to attack
+        raise HTTPException(status_code=401, detail="[Error]: Invalid credentials")
+
+    # BE doesn't need to send token back, only need to verify FE token
+    # FE refresh token is received directly from Firebase, invalid after logout
+    return {"success": True, "user": user}
+
+
+# @app.post("/api/auth/logout", status_code=200)
+# async def register(request: Dict[Any, Any]):
+#     vData = deepcopy(request)
+#     vError = {}
+#     # [1]: Validate request body
+#     if not validate(vData, "token", str, int, vError, required=True):
+#         raise HTTPException(status_code=400, detail=vError["description"])
+#     log(f"[Debug]: Converted data:\n {vData}")
+
+#     # [2]: TODO validate token
+
+#     # [3]: Insert user to DB if not existed
+#     return {"success": True}
