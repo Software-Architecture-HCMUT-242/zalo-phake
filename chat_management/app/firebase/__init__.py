c313ce935fa319d158800969212ec533532097a7
Phước <37379686+vanphuoc3012@users.noreply.github.com>
2025-04-18 23:27:01 +0700

    Refactor Firebase integration and add user info retrieval; enhance conversation name handling (#48)

--- chat_management/app/firebase/__init__.py
+++ chat_management/app/firebase/__init__.py
@@ -1,7 +1,9 @@
+import firebase_admin
+import google.cloud.firestore
 from firebase_admin import db
 
 from .firebase import FirebaseDB
 
 firebase_db = FirebaseDB()
-realtime_db = firebase_db.get_realtime_db()
-firestore_db: db = firebase_db.get_firestore_db()
+realtime_db: firebase_admin.db = firebase_db.get_realtime_db()
+firestore_db: google.cloud.firestore.Client = firebase_db.get_firestore_db()
---
+++ chat_management/app/users/users_db.py
@@ -0,0 +1,11 @@
+from chat_management.app.firebase import realtime_db
+
+DB_USER_PATH = "/User/"
+
+def get_user_info(user_id: str) -> dict:
+    """
+    Get user data from Firebase Realtime Database by user ID.
+    """
+    user_ref = realtime_db.reference(DB_USER_PATH + user_id)
+    user_data = user_ref.get()
+    return user_data
